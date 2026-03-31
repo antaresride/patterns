be1c30b4f9a9fe0626f09cb6c172ac8b1919477e
Antares <antares@antares.com>
2026-03-31 11:33:37 -0700

    Fresh start

---
+++ go.mod
@@ -0,0 +1,6 @@
+module genpay/payment
+
+go 1.26.1
+
+require (
+)
---
+++ payment.go
@@ -0,0 +1,12 @@
+package main
+
+type Payment struct {
+	who string
+}
+type Provider interface {
+	blockchain() string
+}
+
+func main() {
+
+}
